68f0ac53d07e648c9cf91af5ec45a5732ed420de
guixiong.ruan <guixiong.ruan@mobvista.com>
2017-12-25 15:13:28 +0800

    TP-3.2

---
+++ Mongo.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017-10-17
+ * Time: 15:37
+ */
+class Mongo
+{
+
+    private function __construct()
+    {
+
+    }
+
+}
---
+++ Public/test.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017-12-25
+ * Time: 15:01
+ */
+$cache = S('name', array('111111'));
